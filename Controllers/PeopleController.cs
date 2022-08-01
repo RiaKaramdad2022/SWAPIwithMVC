705dfcce9e19a9e626922e1b8dff013c348dbf7c
RiaKaramdad2022 <floriakaramdad2022@gmail.com>
2022-08-01 16:36:30 -0400

    Added Styling

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -5,15 +5,33 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SwapiMVC.Models;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace SwapiMVC.Controllers
 {
     // The class controller brings in MVC functionality like view() which allows us to connect our controller code to our .cshtml files.
     public class PeopleController: Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
+        // page is an optional route parameter that will help us load the correct page based on a given value.
+        public async Task<IActionResult> Index(string page)
+        {
+            //Over here, we're declaring a new string called route that creates the endpoint we'd like to hit. 
+            // It's also adding our page query parameter to the route so as to request the appropriate page's data.
+            // The code inside curly braces page ?? "1" let's us pass in the value of page, our parameter, unless page is null in which case it passes in the value "1".
+            string route = $"people?page={page ?? "1"}";
+            // here we are then pass this route into our _httpClient's GetAsync method and capture the response
+            HttpResponseMessage response = await _httpClient.GetAsync(route);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var people = JsonSerializer.Deserialize<ResultViewModel<PeopleViewModel>>(responseString);
+
+            return View(people);
         }
     }
 }
--- Models/ResultViewModel.cs
+++ Models/ResultViewModel.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace SwapiMVC.Models
 {
-    public class ResultViewModel
+    public class ResultViewModel<TResult>
     {
-        
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
+        [JsonPropertyName("results")]
+        public IEnumerable<TResult> Result { get; set; }
+    
+    public string NextPageNum => Next?.Split("?page=").LastOrDefault();
+    public string PreviousPageNum => Previous?.Split("?page=").LastOrDefault();
     }
+
 }
